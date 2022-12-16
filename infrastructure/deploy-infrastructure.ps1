e85a0ea66b55bb41694815e0b108411197adb287
TNgonephetsy-insight <116840552+TNgonephetsy-insight@users.noreply.github.com>
2022-12-16 12:37:02 -0500

    AB#40 update and reorganized script
    - reorganized order of code execution for readability
    - formatted webapp configuration strings and added connection string

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -10,11 +10,12 @@ $planName = "fabmedical-plan-" + $studentprefix
 $location1 = "eastus"
 $location2 = "westus"
 $appInsights = "fabmedicalai-" + $studentprefix
+$ghcrUser = "tngonephetsy-insight"
 
-#First create a group
+#create Resource Group
 $rg = az group create --name $resourcegroupName --location $location1 | ConvertFrom-Json 
 
-#Then create a CosmosDB
+#create CosmosDB
 az cosmosdb create --name $cosmosDBName `
 --resource-group $resourcegroupName `
 --locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
@@ -23,15 +24,24 @@ az cosmosdb create --name $cosmosDBName `
 --kind MongoDB `
 --server-version 4.0
 
-#Create a Azure App Service Plan
+#setup MongoDB connection string (Thanks to ADunn)
+$cdbConnectionString = az cosmosdb keys list -n $cosmosDBName -g $resourceGroupName --type connection-strings `
+     --query "connectionStrings[?description=='Primary MongoDB Connection String'].connectionString"
+$manipulate = $cdbConnectionString[1]
+$manipulate = $manipulate.Split("""")[1]
+$manipulate = $manipulate.Split("?")
+$cdbConnection = $manipulate[0] + "contentdb?" + $manipulate[1]
+
+#create Azure App Service Plan
 az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
 
+#Azure webapp configurations
+az webapp config appsettings set --settings MONGODB_CONNECTION="$cdbConnection" --name $($webappName) --resource-group $($resourcegroupName)
 az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_URL="https://ghcr.io" --name $($webappName) --resource-group $($resourcegroupName) 
 az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_USERNAME="notapplicable" --name $($webappName) --resource-group $($resourcegroupName) 
 az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_PASSWORD="$($env:CR_PAT)" --name $($webappName) --resource-group $($resourcegroupName) 
 
-
-#Create a Azure Web App with NGINX container
+#create Azure WebApp with NGINX container
 az webapp create `
 --multicontainer-config-file docker-compose.yml `
 --multicontainer-config-type COMPOSE `
@@ -39,15 +49,17 @@ az webapp create `
 --resource-group $($resourcegroupName) `
 --plan $($planName)
 
+#configure Azure WebApp container & private ghcr repository
 az webapp config container set `
---docker-registry-server-password $($env:CR_PAT) `
 --docker-registry-server-url https://ghcr.io `
 --docker-registry-server-user notapplicable `
---multicontainer-config-file docker-compose.yml `
+--docker-registry-server-password $($env:CR_PAT) `
+--multicontainer-config-file ../docker-compose.yml `
 --multicontainer-config-type COMPOSE `
---name $($webappName) `
---resource-group $resourcegroupName 
+--name $webappName `
+--resource-group $resourcegroupName `
+--enable-app-service-storage true
 
+#add AZ Application insight
 az extension add --name application-insights
 az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName --application-type web --retention-time 120
-
