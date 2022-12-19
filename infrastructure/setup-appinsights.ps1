2fa305f5cc4d9fa19256d3eb10f575f1f7d00de5
TNgonephetsy-insight <116840552+TNgonephetsy-insight@users.noreply.github.com>
2022-12-19 03:08:14 -0500

    Update setup-appinsights.ps1

--- infrastructure/setup-appinsights.ps1
+++ infrastructure/setup-appinsights.ps1
@@ -1,7 +1,7 @@
 $studentsuffix = "trn"
 $resourcegroupName = "fabmedical-rg-" + $studentsuffix
 $location1 = "eastus"
-$appInsights = "fabmedicalai-" + $studentsuffix
+$appInsights = "fabmedical-ai-" + $studentsuffix
 
 az extension add --name application-insights
 $ai = az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName --application-type web --retention-time 120 | ConvertFrom-Json
